ece74b185205666ad15647806ca6bde5f350bbcb
vsheva <valera.sheva@gmail.com>
2022-08-01 20:53:50 +0300

    refactoring

--- src/tasks/components/Task.jsx
+++ src/tasks/components/Task.jsx
@@ -1,5 +1,42 @@
 import React from 'react';
 import classNames from 'classnames';
+import PropTypes from 'prop-types';
+
+const Task = ({ handleTaskStatusChange, handleTaskDelete, id, done, text }) => {
+    return (
+        <li className={classNames('list-item', { 'list-item_done': done })}>
+            <input
+                className="list-item__checkbox"
+                type="checkbox"
+                defaultChecked={done}
+                onChange={() => handleTaskStatusChange(id)}
+            />
+            <span className="list-item__text">{text}</span>
+            <button className="list-item__delete-btn" onClick={() => handleTaskDelete(id)}></button>
+        </li>
+    );
+};
+
+Task.propTypes = {
+    id: PropTypes.string.isRequired,
+    done: PropTypes.bool,
+    text: PropTypes.string,
+    handleTaskStatusChange: PropTypes.func.isRequired,
+    handleTaskDelete: PropTypes.func.isRequired,
+};
+
+Task.defaultProps = {
+    text: '',
+    done: null,
+};
+
+export default Task;
+
+
+
+/**
+import React from 'react';
+import classNames from 'classnames';
 
 const Task = ({ done, text, id, onChange, onDelete }) => {
   //const listItemClasses = classNames('list-item', { 'list-item_done': done });
@@ -19,3 +56,4 @@ const Task = ({ done, text, id, onChange, onDelete }) => {
 };
 
 export default Task;
+*/
